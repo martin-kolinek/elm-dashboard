a0144dd51092ca6a1b5be4cfb01a0ba7979a7d41
Martin Kolinek <martin.kolinek@gmail.com>
2017-02-07 20:48:00 +0100

    Added gulp for building and live reloading

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var elm = require('gulp-elm');
+var plumber = require('gulp-plumber');
+var del = require('del');
+var browserSync = require('browser-sync');
+
+// builds elm files and static resources (i.e. html and css) from src to dist folder
+var paths = {
+    dest: 'dist',
+    elm: 'src/*.elm',
+    staticAssets: 'src/*.{html,css}'
+};
+
+gulp.task('clean', function(cb) {
+    del([paths.dest], cb);
+});
+
+gulp.task('elm-init', elm.init);
+
+gulp.task('elm', ['elm-init'], function() {
+    return gulp.src(paths.elm)
+        .pipe(plumber())
+        .pipe(elm())
+        .pipe(gulp.dest(paths.dest))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('staticAssets', function() {
+    return gulp.src(paths.staticAssets)
+        .pipe(plumber())
+        .pipe(gulp.dest(paths.dest))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "dist/"
+        }
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.elm, ['elm']);
+    gulp.watch(paths.staticAssets, ['staticAssets']);
+});
+
+gulp.task('build', ['elm', 'staticAssets']);
+gulp.task('dev', ['build', 'browser-sync', 'watch']);
+gulp.task('default', ['build']);
