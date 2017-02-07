0feccf842d7b626609f27e9de20c0f37b135e4cf
Martin Kolinek <martin.kolinek@gmail.com>
2017-02-07 21:08:27 +0100

    Centered hello world

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,5 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 main : Program Never Model Msg
 main = beginnerProgram { model = model, view = view, update = update }
@@ -14,4 +15,4 @@ update : Msg -> Model -> Model
 update _ model = model
 
 view : Model -> Html Msg
-view model = div [] [text "Hello world"]
+view model = div [class "content"] [text "Hello world"]
