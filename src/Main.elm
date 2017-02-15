deb9b04062b53ce368fdf9d843d8d05426367f35
Martin Kolinek <martin.kolinek@gmail.com>
2017-02-15 21:03:12 +0100

    Added notes

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Navigation
 import News
 import Calendar
 import Clock
+import Notes
 
 main : Program Never Model Msg
 main = Navigation.program (always LocationChange) { init = init, view = view, update = update, subscriptions = subscriptions }
@@ -11,7 +12,8 @@ main = Navigation.program (always LocationChange) { init = init, view = view, up
 type alias Model = {
         clock: Clock.Model,
         news: News.Model,
-        calendar: Calendar.Model
+        calendar: Calendar.Model,
+        notes: Notes.Model
     }
 
 init : Navigation.Location -> (Model, Cmd Msg)
@@ -27,10 +29,11 @@ initModel : Model
 initModel = {
         clock = Clock.initModel,
         news = News.initModel,
-        calendar = Calendar.initModel
+        calendar = Calendar.initModel,
+        notes = Notes.initModel
     }
 
-type Msg = ClockMsg Clock.Msg | NewsMsg News.Msg | CalendarMsg Calendar.Msg | LocationChange
+type Msg = ClockMsg Clock.Msg | NewsMsg News.Msg | CalendarMsg Calendar.Msg | LocationChange | NotesMsg Notes.Msg
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -38,14 +41,16 @@ update msg model =
         ClockMsg clockMsg -> let (clockModel, clockCmd) = Clock.update clockMsg model.clock in ({ model | clock = clockModel }, clockCmd |> Cmd.map ClockMsg)
         CalendarMsg calMsg -> let (calModel, calCmd) = Calendar.update calMsg model.calendar in ({ model | calendar = calModel }, calCmd |> Cmd.map CalendarMsg)
         NewsMsg newsMsg -> let (newsModel, newsCmd) = News.update newsMsg model.news in ({ model | news = newsModel }, newsCmd |> Cmd.map NewsMsg)
+        NotesMsg notesMsg -> let (notesModel, notesCmd) = Notes.update notesMsg model.notes in ({ model | notes = notesModel}, notesCmd |> Cmd.map NotesMsg)
         LocationChange -> (model, Cmd.none)
 
 view : Model -> Html Msg
-view model = div [class "dashboard-container"] [
-              Clock.view model.clock |> Html.map ClockMsg,
-              News.view model.news |> Html.map NewsMsg,
-              Calendar.view model.calendar |> Html.map CalendarMsg
-             ]
+view model = div [class "dashboard-container"]
+    ([
+         Clock.view model.clock |> Html.map ClockMsg,
+         News.view model.news |> Html.map NewsMsg,
+         Calendar.view model.calendar |> Html.map CalendarMsg
+     ] ++ (List.map (Html.map NotesMsg) (Notes.view model.notes)))
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.batch [
---
+++ src/Notes.elm
@@ -0,0 +1,54 @@
+module Notes exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+type alias Model =
+    {
+        notes: List Note,
+        newNote: String
+    }
+
+type alias Note =
+    {
+        id: Int,
+        currentlyEditing: Bool,
+        content: String
+    }
+
+type Msg = StartEditing Int | EndEditing Int | UpdateNewNote String | CreateNote | DeleteNote Int | UpdateNote Int String
+
+initModel : Model
+initModel =
+    {
+        notes = [],
+        newNote = ""
+    }
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = case msg of
+    UpdateNewNote str -> ({ model | newNote = str}, Cmd.none)
+    CreateNote -> let newNote = { id = findNextId model, currentlyEditing = False, content = model.newNote } in ({ model | notes = newNote :: model.notes, newNote = ""}, Cmd.none)
+    DeleteNote delId -> ({ model | notes = List.filter (\x -> x.id /= delId) model.notes}, Cmd.none)
+    StartEditing id -> ({ model | notes = List.map (tryChangeEditingStatus id True) model.notes}, Cmd.none)
+    EndEditing id -> ({ model | notes = List.map (tryChangeEditingStatus id False) model.notes}, Cmd.none)
+    UpdateNote id str -> ({ model | notes = List.map (tryChangeContent id str) model.notes}, Cmd.none)
+
+findNextId : Model -> Int
+findNextId model = Maybe.withDefault 0 (List.maximum (List.map .id model.notes)) + 1
+
+tryChangeEditingStatus : Int -> Bool -> Note -> Note
+tryChangeEditingStatus id status note = if note.id == id then {note | currentlyEditing = status} else note
+
+tryChangeContent : Int -> String -> Note -> Note
+tryChangeContent id str note = if note.id == id then {note | content = str} else note
+
+view : Model -> List (Html Msg)
+view { newNote, notes } = div [class "dashboard-item"] [textarea [onInput UpdateNewNote, value newNote] [], button [onClick CreateNote] [text "done"]] :: List.map viewNote notes
+
+viewNote : Note -> Html Msg
+viewNote { id, currentlyEditing, content} = div [class "dashboard-item"] (
+      if currentlyEditing
+      then [textarea [onInput (UpdateNote id), value content] [], button [onClick (EndEditing id)] [text "done"]]
+      else [text content, button [onClick (StartEditing id)] [text "edit"], button [onClick (DeleteNote id)] [text "delete"]])
